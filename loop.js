e22b0e705499012237c76962262e6823166b5da7
Ulises Rico <ulisesrico@Ulises-MacBook-Pro-13-inch.local>
2018-07-24 10:48:52 -0700

    added pictures

---
+++ loop.js
@@ -0,0 +1,11 @@
+var animal = ['zebra', 'line', 'tiger', 'elephant', 'monkey'];
+
+var professionals = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+
+var students = ['Emma', 'Yang', 'PepeJ', 'PepeV', 'Ethan', 'Alison', 'Nico', 'Brian', 'Uli', 'Caleb', 'Rawad', 'Hassan', 'Yueyang', 'Lyla'];
+
+var numbers = [12, 457, 52, 16];
+
+var app = student.forEach(function(student){
+    -&{student} is making an App
+    }
